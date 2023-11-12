d580fcd9a8369e471f273c4911eb5e25d3ac9740
ssmora19 <133176807+ssmora19@users.noreply.github.com>
2023-11-12 00:47:46 -0500

    front-end implementation in JSF

---
+++ NotesFront/src/main/webapp/js/popup.js
@@ -0,0 +1,9 @@
+function closeCreatePopup(){
+	var createpopup=document.getElementById("createpopup");
+    createpopup.classList.remove("open-popup");
+}
+function openCreatePopup(){
+	var createpopup=document.getElementById("createpopup");
+    createpopup.classList.add("open-popup");
+}
+
